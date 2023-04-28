3337a8d17dd16e6cf346a1c7f4f6b6af836339a3
董飞祥 <fdong26@gmail.com>
2023-04-28 23:31:48 +0800

    自动深色

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,6 +75,11 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: 'Minimalism 文档',
         logo: {
